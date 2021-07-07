5180d03f355135573bf902a367998a42e67f7f7d
mykolapasko <nikpasko@gmail.com>
2021-07-07 22:41:28 +0300

    feat: add max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    return 0;  
+  }
+  return array.sort((a, b) => a - b)[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    return 0;  
+  }
+  return array.sort((a, b) => b - a)[0]  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    return 0;  
+  }
+  return array.reduce((a, b) => a + b) / array.length
 }
